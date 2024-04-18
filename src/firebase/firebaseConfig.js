9a872406a6b56143ed2d00862289d3f323805f96
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-04-18 20:07:10 +0500

    papka

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBhwOz8iuCHQPKwdDoNM2mfAQAbWb5TSm4",
+  authDomain: "my-kitchen-58954.firebaseapp.com",
+  projectId: "my-kitchen-58954",
+  storageBucket: "my-kitchen-58954.appspot.com",
+  messagingSenderId: "758307142493",
+  appId: "1:758307142493:web:21f53c68e112c4c2bcc6b4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
