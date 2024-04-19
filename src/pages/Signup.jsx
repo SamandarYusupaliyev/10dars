1ac88828e941ccb1db2150abe4b03646865c278a
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-04-19 07:29:32 +0500

    papka

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,7 +1,12 @@
+import { signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
+
 function Signup() {
-    return (
-      <div>Sigup</div>
-    )
-  }
-  
+  signInWithPopup (auht,provider)
+  return (
+    <div>Sigup</div>
+  )
+}
+
 export default Signup
+
