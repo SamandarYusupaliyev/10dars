28e1be99af68f3c9b579f727b1e6acdd595de770
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-04-22 13:52:46 +0500

    papka

--- src/pages/Create.jsx
+++ src/pages/Create.jsx
@@ -1,11 +1,17 @@
 import { Form } from "react-router-dom"
 import FormInput from "../components/FormInput"
+import { useState } from "react"
 
 // action
-
-
 function Create() {
-  return (
+ const [Inredients,setIngredients]=useState([])
+ const addIngredient =(e)=>{
+ e.preventDefault()
+}
+
+
+
+return (
    <div className="grid place-items-center">
       <div className="max-w-96 w-full">
          <h1 className="text-3xl text-center font-bold">Creata New Recipe</h1>
@@ -14,11 +20,11 @@ function Create() {
                <div className="flex justify-center flex-col">
                  <div className="flex items-center gap-5 w-full">
                  <FormInput name="title" label="Ingredient" type="text"/>
-                 <button className="btn btn-secondary mt-5">Add</button>
+                 <button onClick={addIngredient}  className="btn btn-secondary mt-5">Add</button>
                  </div>
                  <p className="text-left -mt-2 mb-3">
                    Inredients
-                  <span>:Tuz,Go'sht</span>
+                  <span>:Tuz Go'sht</span>
                  </p>
                </div>
             <FormInput name="cooking Time" label="Cooking Time" type="number"/>
