9efa284406d03de6ce3513d06fa4a5b2bb2d0300
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-04-23 12:05:19 +0500

    papka

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,42 @@
 import { useCollection } from "../hooks/useCollection"
 import RecipiesList from "../components/RecipiesList";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function Home() {
 
+// const [setRecipies] =useState(null)
 
+// useEffect(()=>{
+//   fetch("http://localhost:3000/recipies")
+//   .then((data)=>{
+//     return data.json()
+//   })
+//   .then((recipies)=>{
+//     setRecipies(recipies)
+//   })
+//   .catch((error)=>{
+//     console.log(error);
+//   })
+// })
 
-
-
-
-const deleteRecipie =(id)=>{
-  fetch("http://localhost:3000/recipies/"+id,{
-    method:"DELEtE",
-  }).then((data)=>{
-    return data.json()
-  }).then((data)=>{
-    console.log(data);
-  })
-  .catch((error)=>console.log(error))
-}
-
-
-
-
-
-
-
-
-
+// const [check,setCheck] =useState(0)
+// const deleteRecipie =(id)=>{
+//   fetch("http://localhost:3000/recipies/"+id,{
+//     method:"DELEtE",
+//   }).then((data)=>{
+//     return data.json()
+//   }).then((data)=>{
+//     setCheck(Math.random())
+//   })
+//   .catch((error)=>console.log(error))
+// }
 
   const {data:recipies} =useCollection()
   console.log(recipies && recipies.length);
   return(
   <div>
     <h1 className="text-3xl font-bold">All Recipies-{recipies && recipies.length}</h1>
-    {recipies && <RecipiesList recipies={recipies}/>}
+    {recipies && <RecipiesList recipies={recipies} deleteRecipie={deleteRecipie}/>}
   </div>
   )
 }
