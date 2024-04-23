521f03b953f91d71ab612c2a95fa3ce9c9fabc23
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-04-23 09:58:06 +0500

    papka

--- src/pages/SingleRecipe.jsx
+++ src/pages/SingleRecipe.jsx
@@ -23,8 +23,19 @@ function SingleRecipe() {
     <>
     {data && (
       <div>
-        <h1  className="text-3xl mb-4">{data.title}</h1>
-        <img className="w-full h-[460px]" src={data.image} alt="" />
+        <h1  className="text-3xl mb-4 font-bold">{data.title}</h1>
+        <img className="w-full h-[450px] mb-6" src={data.image} alt="" />
+         <div className="flex gap-4 items-center mb-4">
+          <h2 className="text-xl font-bold items-center">Ingredients:</h2>
+              <span className=" font-normal  bg-slate-500 text-white w-20 text-center h-7  rounded-3xl cursor-pointer">guruch</span>
+              <span className=" font-normal  bg-slate-500 text-white w-20 text-center h-7  rounded-3xl cursor-pointer">piyoz</span>
+              <span className=" font-normal  bg-slate-500 text-white w-20 text-center h-7  rounded-3xl cursor-pointer">yog'</span>
+              <span className=" font-normal  bg-slate-500 text-white w-20 text-center h-7  rounded-3xl cursor-pointer">go'sht</span>
+         </div>
+         <div className="flex items-center gap-4 mb-4">
+           <h2 className="text-xl font-bold">Cooking time:</h2>
+           <span className=" font-normal  bg-slate-500 text-white w-24 text-center h-7  rounded-3xl cursor-pointer">60 minuts</span>
+         </div>
       </div>
     )}
    </>
