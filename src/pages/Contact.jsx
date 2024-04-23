2e15e446991e1b3a3b1bf17cfc438fbf8088bea7
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-04-23 12:43:10 +0500

    papka

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,6 +1,7 @@
 function Contact() {
   return(
-      <div className="flex items-center justify-center">
+      <div className="flex items-center justify-center flex-col ">
+        <h1 className="text-4xl font-bold w-[540px] text-center">Sevimli restoranlaringizdan <span className="text-4xl font-bold text-violet-700">tez ovqat yetkazib berish</span></h1>
         <button className="btn" onClick={()=>document.getElementById('my_modal_5').showModal()}>open modal</button>     
          <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
         <div className="modal-box">
@@ -21,7 +22,7 @@ function Contact() {
 </div>
 <div className="chat chat-end">
   <div className="chat-image avatar">
-    <div className="w-10 rounded-full">
+    <div className="w-10 rounded-full ">
       <img alt="Tailwind CSS chat bubble component" src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
     </div>
   </div>
